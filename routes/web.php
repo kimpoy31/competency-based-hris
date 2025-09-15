e9acd2d2b2b68afab0e91fa5389c24b6319512ff
kimpoy31 <pinanonangkim000@gmail.com>
2025-09-15 15:05:38 +0800

    install daisyUi

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
 
-// Route::middleware(['auth', 'verified'])->group(function () {
-//     Route::get('dashboard', function () {
-//         return Inertia::render('dashboard');
-//     })->name('dashboard');
-// });
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', function () {
+        return Inertia::render('Auth/Login');
+    })->name('login');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', function () {
+        return Inertia::render('welcome');
+    })->name('home');
+});
 
 // require __DIR__.'/settings.php';
 // require __DIR__.'/auth.php';
